201466019b8c1336a8a1637ebb831ac2a21c54b1
Kunal Nawariya <146311711+NawariyaKunal262004@users.noreply.github.com>
2026-09-08 22:02:42 +0530

    added api documentation

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ require('dotenv').config();
 require('./Models/db');
 const PORT = process.env.PORT || 8080;
 
+module.exports = app;
+
+app.get('/', (req, res) => {
+    res.json({
+        status: "success",
+        message: "Welcome to the Expense Tracker API!",
+        version: "1.0.0",
+        documentation: "https://github.com/NawariyaKunal262004/Expense-Tracker-API/tree/main#readme"
+    });
+});
+
 app.get('/ping', (req, res) => {
     res.send('PONG');
 });
